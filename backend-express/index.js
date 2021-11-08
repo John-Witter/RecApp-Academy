717c8af4ba429776277e7593f2486f4cca5401f8
John-Witter <mrsolginski@gmail.com>
2021-11-08 16:33:16 -0500

    http requests in routes => questions.js

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,14 +1,15 @@
 import express from "express";
-// import router from "./routes/questions";
+import { questions } from "./routes/questions.js";
 const app = express();
 
 app.use(express.json());
 
-
-
 app.get("/firstwordsentviainternet", (req, res) => {
     res.send("lo");
 });
+
+app.use('/api/questions', questions);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port: ${port}...`));
--- backend-express/routes/questions.js
+++ backend-express/routes/questions.js
@@ -3,17 +3,32 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    
+    const questions = [
+        { _id: 1, name: "Vladimir Harkonnen", content: "Am I the drama?" },
+        {
+            _id: 2,
+            name: "Lady Jessica",
+            content: "Is Paul the Kwisatz Haderach?",
+        },
+        {
+            _id: 3,
+            name: "Paul Atreides",
+            content: "Why are my dreams so sandy?",
+        },
+    ];
+    res.json(questions);
 });
 
 
 router.post('/', (req, res) => {
-
+    const question = req.body;
+    res.send(question);
 });
 
 
-router.delete('/', (req, res) => {
-
+router.delete('/:question_id', (req, res) => {
+    const _id = req.params.question_id;
+    res.send(_id);
 });
 
-export default router;
+export const questions = router;
