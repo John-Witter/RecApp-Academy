51059fb79baf8bc4b888f915fc8d113ea2fa49af
John-Witter <mrsolginski@gmail.com>
2021-11-08 17:15:45 -0500

    successfully connecting to MONGO_URI in index.js

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,15 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 import { questions } from "./routes/questions.js";
+import { MONGO_URI } from "./config/keys.js";
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('mongoDB Connected!'))
+    .catch(e => console.log(e))
 
 app.get("/firstwordsentviainternet", (req, res) => {
     res.send("lo");
 });
 
-app.use('/api/questions', questions);
-
+app.use("/api/questions", questions);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port: ${port}...`));
--- backend-express/routes/questions.js
+++ backend-express/routes/questions.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { Question } from '../models/question';
+import { Question } from '../models/question.js';
 
 const router = express.Router();
 
