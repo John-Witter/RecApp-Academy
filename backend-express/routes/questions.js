8e4d54e993ebc427e92b091310504c5aca17f2d8
John-Witter <mrsolginski@gmail.com>
2021-11-08 16:54:17 -0500

    built out routes in questions.js

--- backend-express/routes/questions.js
+++ backend-express/routes/questions.js
@@ -1,34 +1,47 @@
 import express from 'express';
+import { Question } from '../models/question';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const questions = [
-        { _id: 1, name: "Vladimir Harkonnen", content: "Am I the drama?" },
-        {
-            _id: 2,
-            name: "Lady Jessica",
-            content: "Is Paul the Kwisatz Haderach?",
-        },
-        {
-            _id: 3,
-            name: "Paul Atreides",
-            content: "Why are my dreams so sandy?",
-        },
-    ];
-    res.json(questions);
+    // const questions = [
+    //     { _id: 1, name: "Vladimir Harkonnen", content: "Am I the drama?" },
+    //     {
+    //         _id: 2,
+    //         name: "Lady Jessica",
+    //         content: "Is Paul the Kwisatz Haderach?",
+    //     },
+    //     {
+    //         _id: 3,
+    //         name: "Paul Atreides",
+    //         content: "Why are my dreams so sandy?",
+    //     },
+    // ];
+    // res.json(questions);
+
+    // get each Question
+    Question.find()
+        .then((questions) => res.json(questions)) // send json of questions
+        .catch(e => res.status(404).json(e))
 });
 
 
 router.post('/', (req, res) => {
-    const question = req.body;
-    res.send(question);
+    const newQuestion  = new Question({
+        name: req.body.name,
+        content: req.body.content
+    })
+
+    newQuestion.save()
+        .then(question => res.json(question))
+        .catch(e => res.status(404).json(e))
 });
 
 
 router.delete('/:question_id', (req, res) => {
-    const _id = req.params.question_id;
-    res.send(_id);
+        Question.findOneAndDelete({_id: req.params.question_id})
+        .then(question => res.json({_id: question._id}))
+        .catch(e => res.status(404).json(e))
 });
 
 export const questions = router;
