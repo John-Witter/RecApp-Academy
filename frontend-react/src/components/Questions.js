7a7f08f0d74c2db6102b0e512fa46b7f78e1ed08
John-Witter <mrsolginski@gmail.com>
2021-11-10 16:53:36 -0500

    finishing react fe

--- frontend-react/src/components/Questions.js
+++ frontend-react/src/components/Questions.js
@@ -2,5 +2,20 @@ import React from "react";
 import '../index.css';
 
 export const Questions = ({ questions, deleteQuestion}) => {
-    return 'Questions Go Here';
+    
+    const generateQuestionsList = () => {
+        if (!questions.length) return <h1>No Questions Yet</h1>
+
+        return questions.map((question, i) => 
+        <div key={i} className="module" >
+            <h3>{question.content}</h3>
+            <span>- {question.name}</span>
+            <button onClick={() => deleteQuestion(question._id)}>Delete</button>
+        </div>).reverse()
+    }
+
+    return (
+        generateQuestionsList()
+        
+    )
 }
