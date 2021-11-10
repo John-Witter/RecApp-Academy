b911e769269e0fffd270ceb13e39ec35c988cd5a
John-Witter <mrsolginski@gmail.com>
2021-11-10 15:27:16 -0500

    Building out React components architecture w/in App.js

---
+++ frontend-react/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+// import { QuestionForm } from './QuestionForm';
+// imoprt { Question } from './Questions';
+
+export const App = () => {
+    /** Going to need:
+     *     QuestionForm component
+     *     Questions component
+     *
+     *     questions array (holds our qs while we wait for DB)
+     *     header
+     *
+     *     deleteQuestion
+     *     submitQuestion
+     */
+
+    return (
+        <div className="App">
+            <header></header>
+        </div>
+    );
+};
--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -1,1 +1,7 @@
-console.log('Hello Worlt!')
+import React from "react";
+import ReactDom from 'react-dom';
+import { App } from './components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDom.render(<App />, document.getElementById('root'))
+})
