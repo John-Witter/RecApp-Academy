7b71771d70f318f82a600da4737994343a92c64d
John-Witter <mrsolginski@gmail.com>
2021-11-10 17:13:41 -0500

    creating global state in App.js

--- frontend-state/src/components/App.js
+++ frontend-state/src/components/App.js
@@ -1,52 +1,65 @@
 import React, { useState } from "react";
-import { QuestionForm } from './QuestionForm';
-import { Question, Questions } from './Questions';
-import '../css_reset.css'
-import '../index.css'
-
+import { QuestionForm } from "./QuestionForm";
+import { Question, Questions } from "./Questions";
+import "../css_reset.css";
+import "../index.css";
 
 export const App = () => {
-    /** Going to need:
-     *     QuestionForm component
-     *     Questions component
-     *
-     *     questions array (holds our qs while we wait for DB)
-     *     header
-     *
-     *     deleteQuestion
-     *     submitQuestion
+    /** New States
+     * user
+     * answer
+     * ui
      */
 
-    const [questions, setQuestions] = useState([
-        { _id: 1, name: "Vladimir Harkonnen", content: "Am I the drama?" },
-        {
-            _id: 2,
-            name: "Lady Jessica",
-            content: "Is Paul the Kwisatz Haderach?",
+    const [GLOBAL_STATE, setGlobalState] = useState({
+        user: {
+            name: "Mark",
+            isAdmin: true
         },
-        {
-            _id: 3,
-            name: "Paul Atreides",
-            content: "Why are my dreams so sandy?",
-        },
-    ]);
-
+        questions: [
+            { _id: 1, name: "Vladimir Harkonnen", content: "Am I the drama?" },
+            {
+                _id: 2,
+                name: "Lady Jessica",
+                content: "Is Paul the Kwisatz Haderach?",
+            },
+            {
+                _id: 3,
+                name: "Paul Atreides",
+                content: "Why are my dreams so sandy?",
+            },
+        ],
+        answers: [],
+        ui: {
+            isDarkMode: false,
+            isLoading
+        }
+    });
 
     // submit handler
-    const submitQuestion = question => {
-        setQuestions([...questions, question])
-    }
+    const submitQuestion = (question) => {
+        setQuestions([...questions, question]);
+    };
 
     // delete handler
-    const deleteQuestion = _id => {
-        setQuestions(questions.filter(question => question._id !== _id));
-    }
+    const deleteQuestion = (_id) => {
+        setQuestions(questions.filter((question) => question._id !== _id));
+    };
 
     return (
         <div className="App">
             <header>RecApp2.0: Frontend + React</header>
-            <QuestionForm submitQuestion={submitQuestion} />
-            <Questions questions={questions} deleteQuestion={deleteQuestion} />
+            <QuestionForm
+                submitQuestion={submitQuestion}
+                //user.name
+                // ui
+            />
+            <Questions
+                questions={questions}
+                deleteQuestion={deleteQuestion}
+                //answers
+                // user.name
+            />
         </div>
     );
 };
